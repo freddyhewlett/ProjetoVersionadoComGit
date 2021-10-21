31ed4bf051c45a4c0aae3f42b0493e81bde7f4eb
Federico Arciniegas <farcinih@everis.com>
2021-10-21 13:41:30 -0300

    Adicionado comando para imprimir a data de hoje.

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace ProjetoVersionadoComGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine(DateTime.Now.ToLongDateString());
         }
     }
 }
