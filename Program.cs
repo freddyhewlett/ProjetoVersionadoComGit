c66e9b52a6cf50c55a1dbe031082a610a635f041
Federico Arciniegas <farcinih@everis.com>
2021-10-21 13:45:27 -0300

    Adicionada nova linha incluindo a versão da aplicação.

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace ProjetoVersionadoComGit
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine(DateTime.Now.ToLongDateString());
+            Console.WriteLine("Versão da aplicação: 1.0");
         }
     }
 }
